a17587b92c6cb8cd611980047fb7337b05b10570
Thyildirim <taha0yldrm@outlook.com>
2025-01-15 21:46:03 +0300

    x

--- rsa_çözüm/script.js
+++ rsa_çözüm/script.js
@@ -1,5 +1,23 @@
+const publicKey = `-----BEGIN PUBLIC KEY-----
+YOUR_PUBLIC_KEY_HERE
+-----END PUBLIC KEY-----`;
+
+const privateKey = `-----BEGIN PRIVATE KEY-----
+YOUR_PRIVATE_KEY_HERE
+-----END PRIVATE KEY-----`;
+
+function encryptData() {
+    const plainData = document.getElementById('input-plain-data').value;
+    const encrypt = new JSEncrypt();
+    encrypt.setPublicKey(publicKey);
+    const encrypted = encrypt.encrypt(plainData);
+    document.getElementById('output-encrypted-data').value = encrypted;
+}
+
 function decryptData() {
-    const inputEncryptedData = document.getElementById('input-encrypted-data').value;
-    const decryptedData = atob(inputEncryptedData); // Basit bir Base64 çözümleme örneği
-    document.getElementById('output-decrypted-data').value = decryptedData;
+    const encryptedData = document.getElementById('input-encrypted-data').value;
+    const decrypt = new JSEncrypt();
+    decrypt.setPrivateKey(privateKey);
+    const decrypted = decrypt.decrypt(encryptedData);
+    document.getElementById('output-decrypted-data').value = decrypted;
 }
